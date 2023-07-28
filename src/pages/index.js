56a5105e2ca5282e471ef04d6123a94458a65fdd
tania-dev <83649143+tania-dev@users.noreply.github.com>
2023-07-28 12:50:39 +0600

    layout added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import RootLayout from '@/components/layout/RootLayout';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function HomePage() {
   return (
     <div>
       <h2>
-        
+        h2
       </h2>
     </div>
   )
 }
+
+HomePage.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
